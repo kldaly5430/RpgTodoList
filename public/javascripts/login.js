e8c63ce49e22822556624f5dcc7b0e6bc78d3dc6
kldaly5430 <kldaly5430@gmail.com>
2021-02-12 19:04:16 -0500

    initial commit
    Create project.
    Created login page

---
+++ public/javascripts/login.js
@@ -0,0 +1,10 @@
+const e = require("express");
+
+function showPassword() {
+    var x = document.getElementById("userPassword");
+    if(x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
